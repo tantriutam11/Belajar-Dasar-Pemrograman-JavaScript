31956e5459f0e5172d7f5fa5c8cd22c692cb4337
Tantri <127431736+tantriutam11@users.noreply.github.com>
2023-04-30 21:25:14 +0700

    membuat variabel let dan const

---
+++ variable.js
@@ -0,0 +1,15 @@
+/* pada javascript ada tiga cara untuk mendeklarasikan variabel yaitu menggunakan keyword:
+1. var
+2. let, nilai variabel yang masih dapat diubah
+3. const, nilai variabel bersifat constant sehingga nilai variabel tidak dapat diubah */
+
+// contoh untuk variabel let
+let lastName;
+lastName = "Manalu";
+console.log(lastName);
+
+// contoh untuk variabel const
+const x = 100;
+console.log(x);
+x = 200; // Akan menimbulkan error karna variabel x bersifat const
+console.log(x);
